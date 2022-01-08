b070b5b2a0960580e0f1991e462b752046535442
Null Terminator '\0 <88001942+MohannadShehata@users.noreply.github.com>
2022-01-08 22:59:50 +0000

    Added ?birthday to display a birthday message

--- main_cog.py
+++ main_cog.py
@@ -17,8 +17,9 @@ General commands:
 ?die - This command returns a random last words
 ?quote - This command returns a random quote
 ?ping - This command returns the latency
-
+?birthday - This command displays a special birthday message
 Music commands:
+
 ?play <keywords> - Finds the song on youtube and plays it in your current channel
 ?queue - Displays the current music queue
 ?skip - Skips the current song being played
@@ -40,7 +41,7 @@ No one but me, lozer!
         "Don't listen to Stews. He lies. Hang in there.",
         "Don't listen to Stews. He lies. You are a great person / bot!"
         ]
-        
+        self.age = 19
 
     @commands.command(name="help", help="Displays all the available commands")
     async def help(self, ctx):
@@ -81,4 +82,13 @@ No one but me, lozer!
                 await asyncio.sleep(1)
                 await message.channel.send(choice(self.options))
 
+    @commands.command(name='birthday', help='This command displays a special birthday message')            
+    async def birthday(self,ctx,*args):
+        msg=''
+        if len(args)==0 or args[0]=='<@!143449835771527169>':
+          msg = 'Happy Birthday!!!\n<a:neonpartyblob:929500659055722526> <a:partyCatJam:929500684322238494>'+  '<@143449835771527169>'+'<a:partyCatJam:929500684322238494><a:neonpartyblob:929500659055722526>\n You are '+ str(self.age) + ' years old'
+          self.age+=1
+        else:  
+          msg= 'Happy Birthday!!!\n<a:neonpartyblob:929500659055722526> <a:partyCatJam:929500684322238494>'+ args[0]+'<a:partyCatJam:929500684322238494><a:neonpartyblob:929500659055722526>' 
+        await ctx.send(msg)
 
